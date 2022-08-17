e0ab17b6dda6d30927be2b372dc1a7b77eb358e8
J-Jamet <jeremy.jamet@kunzisoft.com>
2022-08-17 17:30:01 +0200

    feature: Add contribution dialog

---
+++ app/src/main/java/com/kunzisoft/hardware/key/UnderDevelopmentFeatureDialogFragment.kt
@@ -0,0 +1,39 @@
+package com.kunzisoft.hardware.key
+
+import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.SpannableStringBuilder
+import androidx.appcompat.app.AlertDialog
+import androidx.core.text.HtmlCompat
+import androidx.fragment.app.DialogFragment
+
+/**
+ * Custom Dialog that asks the user to download the pro version or make a donation.
+ */
+class UnderDevelopmentFeatureDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        activity?.let { activity ->
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(activity)
+            val stringBuilder = SpannableStringBuilder()
+            stringBuilder.append(HtmlCompat.fromHtml(getString(R.string.html_text_dev_feature), HtmlCompat.FROM_HTML_MODE_LEGACY)).append("\n\n")
+                .append(HtmlCompat.fromHtml(getString(R.string.html_text_dev_feature_contibute), HtmlCompat.FROM_HTML_MODE_LEGACY)).append(" ")
+                .append(HtmlCompat.fromHtml(getString(R.string.html_text_dev_feature_encourage), HtmlCompat.FROM_HTML_MODE_LEGACY))
+            builder.setPositiveButton(R.string.contribute) { _, _ ->
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse(getString(R.string.keepro_url))
+                    )
+                )
+            }
+            builder.setMessage(stringBuilder)
+            // Create the AlertDialog object and return it
+            return builder.create()
+        }
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
--- app/src/main/java/com/kunzisoft/hardware/key/YubikeySettingsFragment.kt
+++ app/src/main/java/com/kunzisoft/hardware/key/YubikeySettingsFragment.kt
@@ -3,6 +3,7 @@ package com.kunzisoft.hardware.key
 import android.os.Bundle
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.kunzisoft.hardware.yubikey.Slot
 
 class YubikeySettingsFragment : PreferenceFragmentCompat() {
@@ -21,6 +22,15 @@ class YubikeySettingsFragment : PreferenceFragmentCompat() {
             setOnPreferenceChangeListener { _, newValue ->
                 slotPreferenceManager.setDefaultSlot(Slot.fromString(newValue.toString()))
                 true
+            }
+        }
+
+        findPreference<SwitchPreferenceCompat>(getString(R.string.virtual_key_pref))?.apply {
+            setOnPreferenceClickListener {
+                this.isChecked = false
+                UnderDevelopmentFeatureDialogFragment()
+                    .show(requireActivity().supportFragmentManager, "underDevFeatureDialog")
+                false
             }
         }
     }
