942a6e72c27593238d39835fd612915783fc42d2
J-Jamet <jeremy.jamet@kunzisoft.com>
2024-05-06 15:13:45 +0200

    fix: Broadcast

--- app/src/main/java/com/kunzisoft/hardware/key/ConnectionManager.kt
+++ app/src/main/java/com/kunzisoft/hardware/key/ConnectionManager.kt
@@ -17,6 +17,8 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.RECEIVER_EXPORTED
 import com.kunzisoft.hardware.yubikey.challenge.VirtualYubiKey
 import com.kunzisoft.hardware.yubikey.challenge.NfcYubiKey
 import com.kunzisoft.hardware.yubikey.challenge.UsbYubiKey
@@ -122,8 +124,8 @@ internal class ConnectionManager(private val activity: Activity) : BroadcastRece
     }
 
     private fun initUSBConnection(activity: Activity) {
-        activity.registerReceiver(this, IntentFilter(ACTION_USB_PERMISSION_REQUEST))
-        activity.registerReceiver(this, IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED))
+        ContextCompat.registerReceiver(activity, this, IntentFilter(ACTION_USB_PERMISSION_REQUEST), RECEIVER_EXPORTED)
+        ContextCompat.registerReceiver(activity, this, IntentFilter(UsbManager.ACTION_USB_DEVICE_ATTACHED), RECEIVER_EXPORTED)
 
         val usbManager = activity.getSystemService(Context.USB_SERVICE) as UsbManager
         for (device in usbManager.deviceList.values) {
@@ -176,7 +178,7 @@ internal class ConnectionManager(private val activity: Activity) : BroadcastRece
             return
         }
         unplugReceiver = receiver
-        context.registerReceiver(this, IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED))
+        ContextCompat.registerReceiver(context, this, IntentFilter(UsbManager.ACTION_USB_DEVICE_DETACHED), RECEIVER_EXPORTED)
     }
 
     fun registerUsbPermissionDeniedReceiver(receiver: UsbPermissionDeniedReceiver) {
@@ -279,7 +281,9 @@ internal class ConnectionManager(private val activity: Activity) : BroadcastRece
         val usbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
 
         var flags = PendingIntent.FLAG_UPDATE_CURRENT
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            flags = PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             flags = PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         }
 
